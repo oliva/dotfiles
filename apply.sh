04ba6d28971bf86bbe1eb5f04a9832f1efba595d
Temlin Olivér <git@teml.in>
2020-10-06 10:57:26 +0200

    script: fix POSIX compatibility

--- apply.sh
+++ apply.sh
@@ -10,7 +10,7 @@ log=0 # -1: quiet 0: normal 1: no-run 2: debug
 modules=""
 
 # parse arguments
-while [ "$@" ]; do
+while [ -n "${1:-}" ]; do
 	case $1 in
 		-q|--quiet)
 			log=-1
@@ -43,56 +43,55 @@ done
 
 # iterate over directories
 for cmd in $modules; do
-	# continue if command exists
-	if type -p $cmd > /dev/null; then
+	# skip if command is not available
+	type -p $cmd > /dev/null || continue
 
-		# iterate over files
-		for file in $(find "$stowdir/$cmd" -type f -printf '%P\n'); do
+	# iterate over files
+	for file in $(find "$stowdir/$cmd" -type f -printf '%P\n'); do
 
-			# get relative symlink path
-			destdir="$(dirname "$target/$file")"
-			relpath="$(realpath --relative-to="$destdir" "$stowdir/$cmd/$file")"
-			[ $log -ge 2 ] && {
-				echo src "$stowdir/$cmd/$file"
-				echo dst "$target/$file"
-				echo lnk "$relpath"
-			}
+		# get relative symlink path
+		destdir="$(dirname "$target/$file")"
+		relpath="$(realpath --relative-to="$destdir" "$stowdir/$cmd/$file")"
+		[ $log -ge 2 ] && {
+			echo src "$stowdir/$cmd/$file"
+			echo dst "$target/$file"
+			echo lnk "$relpath"
+		}
 
-			# ensure parent dir
-			if [ ! -d "$destdir" ]; then
-				[ $log -ge 1 ] && echo mkdir -pv "$destdir"
-				[ $log -eq 0 ] &&      mkdir -pv "$destdir"
-				[ $log -lt 0 ] &&      mkdir -p  "$destdir"
-			else
-				[ $log -ge 2 ] && echo dir "$destdir" already exists
+		# ensure parent dir
+		if [ ! -d "$destdir" ]; then
+			[ $log -ge 1 ] && echo mkdir -pv "$destdir"
+			[ $log -eq 0 ] &&      mkdir -pv "$destdir"
+			[ $log -lt 0 ] &&      mkdir -p  "$destdir"
+		else
+			[ $log -ge 2 ] && echo dir "$destdir" already exists
+		fi
+
+		# add link
+		destbase="$(basename "$target/$file")"
+		# skip placeholder files
+		if [ "$destbase" = '_' ]; then
+			[ $log -ge 2 ] && echo skipping placeholder "'$file'"
+		else
+
+			# don't overwrite if files differ
+			if [ -e "$target/$file" ] && ! diff -q "$target/$file" "$stowdir/$cmd/$file" > /dev/null; then
+				echo target "$target/$file" and source "$stowdir/$cmd/$file" differ, not replacing
+				diff "$target/$file" "$stowdir/$cmd/$file"
+				continue
+			fi
+			reallink=$(stat --printf %N "$target/$file" |grep -o " -> '.*'$" |grep -o "'.*'" |grep -o "[^']*" || true)
+
+			# skip if link already exists
+			if [ "$reallink" = "$relpath" ]; then
+				[ $log -ge 1 ] && echo link for "$file" is in place
+				continue
 			fi
 
-			# add link
-			destbase="$(basename "$target/$file")"
-			# skip placeholder files
-			if [ "$destbase" = '_' ]; then
-				[ $log -ge 2 ] && echo skipping placeholder "'$file'"
-			else
-
-				# don't overwrite if files differ
-				if [ -e "$target/$file" ] && ! diff -q "$target/$file" "$stowdir/$cmd/$file" > /dev/null; then
-					echo target "$target/$file" and source "$stowdir/$cmd/$file" differ, not replacing
-					diff "$target/$file" "$stowdir/$cmd/$file"
-					continue
-				fi
-				reallink=$(stat --printf %N "$target/$file" |grep -o " -> '.*'$" |grep -o "'.*'" |grep -o "[^']*" || true)
-
-				# skip if link already exists
-				if [ "$reallink" = "$relpath" ]; then
-					[ $log -ge 1 ] && echo link for "$file" is in place
-					continue
-				fi
-
-				# we can safely apply our new files
-				[ $log -ge 1 ] && echo ln -sfv "$relpath" "$target/$file"
-				[ $log -eq 0 ] &&      ln -sfv "$relpath" "$target/$file"
-				[ $log -lt 0 ] &&      ln -sf  "$relpath" "$target/$file"
-			fi
-		done
-	fi
+			# we can safely apply our new files
+			[ $log -ge 1 ] && echo ln -sfv "$relpath" "$target/$file"
+			[ $log -eq 0 ] &&      ln -sfv "$relpath" "$target/$file"
+			[ $log -lt 0 ] &&      ln -sf  "$relpath" "$target/$file"
+		fi
+	done
 done
